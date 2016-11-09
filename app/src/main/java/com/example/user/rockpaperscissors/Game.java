09db3571e38dda7ffb777df066f26263d4f8b401
Paul Milne <hallhill@gmail.com>
2016-11-09 14:23:16 +0000

    Building a Game class for game logic

--- app/src/main/java/com/example/user/rockpaperscissors/Game.java
+++ app/src/main/java/com/example/user/rockpaperscissors/Game.java
@@ -1,8 +1,42 @@
 package com.example.user.rockpaperscissors;
 
+import java.util.Random;
+
 /**
  * Created by user on 09/11/2016.
  */
 
 public class Game {
+
+    private String mPlayerMove;
+    private String mComputerMove;
+    private String[] mPlays;
+
+    public Game(){
+        this.mPlays = new String[3];
+        setUpPlays();
+        this.mPlayerMove = new String();
+        this.mComputerMove = new String();
+    }
+
+    public void setUpPlays(){
+        mPlays[0] = "Rock";
+        mPlays[1] = "Paper";
+        mPlays[2] = "Scissors";
+    }
+
+    public String computerMove() {
+        Random rand = new Random();
+        int randomIndex = rand.nextInt(3);
+        String result = mPlays[randomIndex];
+        return result;
+    }
+
+
+
+    //method to record the player choice
+
+    //call a method to get a random play for the computer
+
+    //method to compare moves and decide a winner
 }
--- app/src/test/java/com/example/user/rockpaperscissors/GameTest.java
+++ app/src/test/java/com/example/user/rockpaperscissors/GameTest.java
@@ -5,4 +5,6 @@ package com.example.user.rockpaperscissors;
  */
 
 public class GameTest {
+
+
 }
